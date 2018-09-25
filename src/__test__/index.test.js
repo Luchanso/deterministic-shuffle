09e95100fc1873fab939e2b5f600900546523e0d
Loutchansky Oleg <luchansodev@gmail.com>
2018-09-25 13:30:06 +0300

    Some test

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -4,27 +4,6 @@ describe('deterministic-shuffle', () => {
   const numbers = [1, 2, 3, 4, 5];
   const startDate = new Date(2017, 0, 1);
 
-  it('should return shuffled array', () => {
-    const actual = shuffle(numbers, startDate);
-    const expected = shuffle(numbers, startDate);
-
-    expect(actual.length).toBe(numbers.length);
-    expect(actual).toEqual(expected);
-  });
-
-  it('should return deterministic shuffled array', () => {
-    Date.now = jest.fn();
-
-    Date.now.mockImplementationOnce(() => new Date(2017, 0, 1));
-    const firstCicle = shuffle(numbers, startDate);
-
-    Date.now.mockImplementationOnce(() => new Date(2017, 0, 2));
-    const secondCicle = shuffle(numbers, startDate);
-
-    expect(firstCicle).toEqual(secondCicle);
-    Date.now.mockRestore();
-  });
-
   it('should return deterministic shuffled array', () => {
     Date.now = jest.fn();
 
